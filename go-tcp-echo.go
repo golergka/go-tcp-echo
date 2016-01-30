bdc5c0dc993b7fce606203055e8937674766ac1a
Max Yankov <golergka@gmail.com>
2016-01-30 23:56:50 +0200

    Logging and gofmt

--- go-tcp-echo.go
+++ go-tcp-echo.go
@@ -1,47 +1,52 @@
 package main
 
 import (
-    "net"
-    "log"
-    "flag"
-    "strconv"
+	"flag"
+	"log"
+	"net"
+	"strconv"
 )
 
 const (
-    CONN_TYPE = "tcp"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
-    host := flag.String("host", "localhost", "The host to run echo serve on.")
-    port := flag.Int("port", 3333, "Port to accept connections on.")
-    flag.Parse();
+	host := flag.String("host", "localhost", "The host to run echo serve on.")
+	port := flag.Int("port", 3333, "Port to accept connections on.")
+	flag.Parse()
 
-    url := *host + ":" + strconv.Itoa(*port)
-    l, err := net.Listen(CONN_TYPE, url)
-    if err != nil {
-        log.Panicln(err)
-    }
-    defer l.Close()
+	url := *host + ":" + strconv.Itoa(*port)
+	l, err := net.Listen(CONN_TYPE, url)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("Listening to connections on", url)
+	defer l.Close()
 
-    for {
-        conn, err := l.Accept()
-        if err != nil {
-            log.Panicln(err)
-        }
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Panicln(err)
+		}
 
-        go handleRequest(conn)
-    }
+		go handleRequest(conn)
+	}
 }
 
 func handleRequest(conn net.Conn) {
-    defer conn.Close()
+	log.Println("Accepted new connection.")
+	defer conn.Close()
+	defer log.Println("Closed connection.")
 
-    for {
-        buf := make([]byte, 1024)
-        size, err := conn.Read(buf)
-        if err != nil {
-            return;
-        }
-        conn.Write(buf[:size])
-    }
+	for {
+		buf := make([]byte, 1024)
+		size, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		data := buf[:size]
+		log.Println("Read new data from connection", data)
+		conn.Write(data)
+	}
 }
