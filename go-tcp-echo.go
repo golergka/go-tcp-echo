62ddf9b30048007c2e502c22174a08e16ba6fbee
Max Yankov <golergka@gmail.com>
2016-01-30 18:38:30 +0200

    Simple receiver

---
+++ go-tcp-echo.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "net"
+    "log"
+)
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "3333"
+    CONN_TYPE = "tcp"
+)
+
+func main() {
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+    if err != nil {
+        log.Panicln(err)
+    }
+    defer l.Close()
+
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+            log.Panicln(err)
+        }
+
+        go handleRequest(conn)
+    }
+}
+
+func handleRequest(conn net.Conn) {
+    defer conn.Close()
+
+    buf := make([]byte, 1024)
+    _, err := conn.Read(buf)
+    if err != nil {
+        log.Panicln(err)
+    }
+
+    conn.Write([]byte("Message received."))
+}
